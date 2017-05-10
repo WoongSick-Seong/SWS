fcd3eb772348c5c43387a95fe31885f83a3e8a4b
seekers_default <seekers_default@DESKTOP-MN2TH63>
2017-05-10 15:38:00 +0900

    마스터가 했다에 1만 넣음

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("아아아아1");
 		System.out.println("qqqq1");
-		System.out.println("마스터가 했다");
+		System.out.println("마스터가 했다1");
 	}
 
 }
